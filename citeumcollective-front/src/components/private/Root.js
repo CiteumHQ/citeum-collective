0ccbee2b0734945884b175a4b903ee54f5d92828
Samuel Hassine <samuel@hassine.fr>
2020-12-09 07:47:42 +0100

    First work

--- citeumcollective-front/src/components/private/Root.js
+++ citeumcollective-front/src/components/private/Root.js
@@ -5,7 +5,6 @@ import React, { useEffect, useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
-import '../../App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import { useBasicQuery } from '../../network/Apollo';
 import Profile from './home/Profile';
--- citeumcollective-front/src/components/public/Root.js
+++ citeumcollective-front/src/components/public/Root.js
@@ -1,17 +1,59 @@
 import React from 'react';
+import * as PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import logo from '../Logo_text.png';
+import logo from '../../resources/images/logo_text_h.png';
+import logoLuatix from '../../resources/images/logo_luatix_text.png';
+import logoLimeo from '../../resources/images/logo_limeo_text.png';
+import initiativeWhite from '../../resources/images/initiative_white.png';
 
-function Root() {
-  return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <div>Welcome to Filigran</div>
-                <div><Link to={'/app'}>Access application</Link></div>
-            </header>
+const styles = () => ({
+  container: {
+    textAlign: 'center',
+    margin: '0 auto',
+    paddingTop: 50,
+  },
+  logo: {
+    width: 400,
+    margin: '0px 0px 30px 0px',
+  },
+  item: {
+    margin: '0 50px 0 50px',
+    display: 'inline-block',
+  },
+  logosContainer: {
+    margin: '100px 0 30px 0',
+  },
+  footer: {
+    textAlign: 'center',
+  },
+  itemLogo: {
+    width: 150,
+  },
+});
+
+const Root = ({ classes }) => (
+        <div className={classes.container}>
+            <img src={logo} className={classes.logo} alt="logo" />
+            <Typography variant='h5'>Welcome Citeum Federation&apos;s workspace.</Typography>
+            <Button variant="contained" color="secondary" style={{ marginTop: 40 }} component={Link} to='/app'>
+                Access to the dashboard
+            </Button>
+            <div className={classes.logosContainer}>
+                <div className={classes.item}><img src={logoLuatix} alt="logo_luatix" className={classes.itemLogo} /></div>
+                <div className={classes.item}><img src={logoLimeo} alt="logo_limeo" className={classes.itemLogo} /></div>
+            </div>
+            <div className={classes.footer}>
+                <hr style={{ border: 0, height: 1, backgroundImage: 'linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0))' }} />
+                <img src={initiativeWhite} alt="initiative" className={classes.itemLogo} />
+            </div>
         </div>
-  );
-}
+);
 
-export default Root;
+Root.propTypes = {
+  classes: PropTypes.object,
+};
+
+export default withStyles(styles)(Root);
